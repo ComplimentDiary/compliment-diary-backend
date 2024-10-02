856faae8e9c3b5b9e14674c15da72bca7d36ee1f
yeji <choyeji1591@gmail.com>
2024-10-02 14:34:52 +0900

    fix : 일기 글 등록 가능 개수 조건 절 변경

--- src/main/java/com/sgyj/complimentdiary/modules/service/DiaryService.java
+++ src/main/java/com/sgyj/complimentdiary/modules/service/DiaryService.java
@@ -40,12 +40,12 @@ public class DiaryService {
 
         User user = userRepository.findById(createDiary.getUserId()).orElseThrow(() -> new NoMemberException("일치하는 회원을 찾을 수 없습니다."));
 
-        UserDiary userDiary = UserDiary.from(user, createDiary.getDate());
+        UserDiary userDiary = userDiaryRepository.findByUserAndDiaryDate(user, createDiary.getDate()).orElse(UserDiary.from(user, createDiary.getDate()));
 
         List<Diary> diaryList =
-            createDiary.getDiaryContentList().stream().map(diaryEntries -> Diary.from(diaryEntries.getContent(),
-                                                                                      diaryEntries.getRating(),
-                                                                                      userDiary)).toList();
+                createDiary.getDiaryContentList().stream().map(diaryEntries -> Diary.from(diaryEntries.getContent(),
+                        diaryEntries.getRating(),
+                        userDiary)).toList();
 
         if (userDiary.getDiaryList().size() + diaryList.size() > MAX_CONTENT_COUNT) {
             throw new ExceedContentException("등록 가능한 콘텐츠 수를 벗어났습니다.");
@@ -57,7 +57,7 @@ public class DiaryService {
 
         if (!createDiary.getImageUrlList().isEmpty()) {
             List<File> fileList =
-                createDiary.getImageUrlList().stream().map(imageUrl -> File.of(userDiary.getId(), imageUrl)).toList();
+                    createDiary.getImageUrlList().stream().map(imageUrl -> File.of(userDiary.getId(), imageUrl)).toList();
             fileRepository.saveAll(fileList);
         }
 
