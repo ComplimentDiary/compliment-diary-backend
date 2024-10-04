5b596df3660c02979a98fc71fb500c531db50428
yeji <choyeji1591@gmail.com>
2024-10-04 16:25:52 +0900

    test : 회원가입 테스트코드 추가

---
+++ src/test/java/com/sgyj/complimentdiary/modules/controller/UserControllerTest.java
@@ -0,0 +1,39 @@
+package com.sgyj.complimentdiary.modules.controller;
+
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.ContentType;
+import com.sgyj.complimentdiary.TestcontainersConfiguration;
+import com.sgyj.complimentdiary.modules.dto.CreateUserRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@Import(TestcontainersConfiguration.class)
+@DisplayName("사용자 api 호출 테스트")
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void sign_up_success() throws Exception {
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUserId("yejiCho");
+        createUserRequest.setPassword("yejiCho");
+        createUserRequest.setEmail("yejiCho@email.com");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users/register")
+                .content(objectMapper.writeValueAsString(createUserRequest))
+                .contentType(ContentType.APPLICATION_JSON.toString())).andExpect(status().isOk());
+    }
+}
